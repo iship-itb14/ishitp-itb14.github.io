74117b5792c5d57b1b9dfdd4664ea4ccec519d10
Ishita Pawar <ishitapawar19@gmail.com>
2026-02-09 21:11:19 -0700

    description change

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ tabLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
 
-        // Remove active from all links & sections
+      
         tabLinks.forEach(l => l.classList.remove('active'));
         tabSections.forEach(s => s.classList.remove('active'));
 
-        // Activate clicked tab
+    
         link.classList.add('active');
         document.getElementById(link.dataset.tab).classList.add('active');
     });
